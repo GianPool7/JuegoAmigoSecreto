a42dc859571ce982fd6cd5d757fd3fec0131b2ea
gian pool <gianpoolalvinocantaro@gmail.com>
2025-01-25 17:00:21 -0500

    agregando nuevas funciones, como boton de reinicar juego , agregar mas , empezar juego , pero aun no me convence la funcion de guardar los datos de los amigos seleccionados

--- app.js
+++ app.js
@@ -8,11 +8,38 @@ let listadoAmigo=[];
 
 let listadoAmigoEscojido=[]
 
+// el input de entrada 
+let nombreAmigo = document.getElementById("amigo");
+nombreAmigo.focus();
 
+// el boton para agregar
+let btnAgregar=document.getElementById("agregar");
+
+// el boton de sortear amigos
+//variable para deshabilitar la opcion de seguir sorteando
+let btn=document.getElementById("btnSortear");
+
+// los botones del juego
+// variable de la caja de empezar
+let btnGo=document.getElementById("btn_caja_empezar");
+let btnContinuar=document.getElementById("continuarAgregando");
+let btnReiniciar=document.getElementById("reiniciar");
+
+
+// variable creada para mostrar el nombre escojido
+let resultadoNombre=document.getElementById("resultado")
+
+// para escuchar el boton 
+
+btn.addEventListener("click",deshabilitar);
+
+//
+function deshabilitar() {
+    btnContinuar.style.display="none";
+}
 
 function agregarAmigo() {
     
-    let nombreAmigo = document.getElementById("amigo");
     let alerta=document.getElementById("mensajeAmigo")
 
     if (nombreAmigo.value==="") {
@@ -28,6 +55,7 @@ function agregarAmigo() {
             console.log(listadoAmigo);
             nombreAmigo.value="";
             nombreAmigo.focus();
+            btnGo.style.display="flex";
         }
     }
 
@@ -39,16 +67,64 @@ function agregarAmigo() {
 function sortearAmigo() {
 
     let amigoElegido=document.getElementById("resultado")
-    amigoElegido=listadoAmigoEscojido[Math.floor(Math.random()*listadoAmigo.length)]
+    amigoElegido=listadoAmigo[Math.floor(Math.random()*listadoAmigo.length)]
 
+
+    console.log(amigoElegido);
+    
     if (listadoAmigoEscojido.includes(amigoElegido)) {
-        alert("el elegido ya esta")
+        amigoElegido=listadoAmigo[Math.floor(Math.random()*listadoAmigo.length)]
+
+        if (listadoAmigoEscojido.length == listadoAmigo.length) {
+            resultadoNombre.innerHTML="Ya se sortearon todos los amigos secretos";
+            btn.setAttribute("disabled","true");
+            btn.style.background="var(--color-text)";
+            btnReiniciar.style.display="flex";
+        }
     }else{
-        alert("el elegido ya se agrego")
         listadoAmigoEscojido.push(amigoElegido)
+        resultadoNombre.innerHTML=amigoElegido
     }
 
-    console.log(listadoAmigo);
+    console.log(listadoAmigoEscojido);
 
 }
 
+
+
+function empezarJuego() {
+    nombreAmigo.setAttribute("disabled","true");
+    btnAgregar.setAttribute("disabled","true");
+    btnGo.style.display="none";
+    btn.style.display="flex";
+    btnContinuar.style.display="flex";
+}
+
+function continuarAgregando() {
+    btnGo.style.display="flex";
+    btn.style.display="none";
+    btnContinuar.style.display="none";
+    nombreAmigo.removeAttribute("disabled","true");
+    btnAgregar.removeAttribute("disabled","true");
+    nombreAmigo.focus();
+}
+
+function reiniciarJuego() {
+    btnContinuar.style.display="none";
+    btnReiniciar.style.display="none";
+    btn.style.display="none";
+    // para que pueda ingresar con normalidad los datos
+    nombreAmigo.removeAttribute("disabled","true");
+    btnAgregar.removeAttribute("disabled","true");
+    nombreAmigo.focus();
+    resultadoNombre.innerHTML="";
+    // limpiando mi arreglo
+    listadoAmigo.length=0;
+    listadoAmigoEscojido.length=0;
+    // Limpiando la pantalla del arreglo
+    let listadoNombresAmigoSecreto=document.getElementById("listaAmigos").innerHTML="";
+    // reiniciando el botn
+    btn.removeAttribute("disabled","true");
+    btn.style.background="var(--color-button)";
+    //
+}
